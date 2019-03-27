e62fdeb81e672d4d614234883ddb7711e6180a8f
Kwoconut <43205572+Kwoconut@users.noreply.github.com>
2019-03-27 14:46:50 +0100

    Update Lists
    Updated ArrayLists view

--- src/view/ViewStore.java
+++ src/view/ViewStore.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,53 +11,12 @@ import viewmodel.ViewModelStore;
 
 public class ViewStore
 {
-
-   @FXML
-   Label label00;
    
    @FXML
-   Label label01;
+   ArrayList<Label> nameLabelList;
    
    @FXML
-   Label label02;
-   
-   @FXML
-   Label label10;
-   
-   @FXML
-   Label label11;
-   
-   @FXML
-   Label label12;
-   
-   @FXML
-   Label label20;
-   
-   @FXML
-   Label label21;
-   
-   @FXML
-   Label label22;
-   
-   @FXML
-   Label label30;
-   
-   @FXML
-   Label label31;
-   
-   @FXML
-   Label label32;
-   
-   @FXML
-   Label label40;
-   
-   @FXML
-   Label label41;
-   
-   @FXML
-   Label label42;
-   
-   
+   ArrayList<Label> priceLabelList;   
 
    private ViewModelStore model;
 
@@ -72,16 +33,11 @@ public class ViewStore
       this.view = view;
       this.scene = scene;
       this.title = title;
-      label00.textProperty().bind(model.getNameProperty(0));
-      label10.textProperty().bind(model.getNameProperty(1));
-      label20.textProperty().bind(model.getNameProperty(2));
-      label30.textProperty().bind(model.getNameProperty(3));
-      label40.textProperty().bind(model.getNameProperty(4));
-      label01.textProperty().bind(model.getPriceProperty(0));
-      label11.textProperty().bind(model.getPriceProperty(1));
-      label21.textProperty().bind(model.getPriceProperty(2));
-      label31.textProperty().bind(model.getPriceProperty(3));
-      label41.textProperty().bind(model.getPriceProperty(4));
+      for (int i = 0 ; i<5;i++)
+      {
+         nameLabelList.get(i).textProperty().bind(viewModel.getNameProperty(i));
+         priceLabelList.get(i).textProperty().bind(viewModel.getPriceProperty(i));
+      }
          
    }
 
