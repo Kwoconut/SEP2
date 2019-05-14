0c7a4863652abb5bebc5b8fa13d5b9d482134967
joelh273 <45686194+joelh273@users.noreply.github.com>
2019-05-14 16:32:53 +0200

    cevaTest

--- src/testing/Testing_Sprint2_Day3_model_Offer.java
+++ src/testing/Testing_Sprint2_Day3_model_Offer.java
@@ -15,8 +15,8 @@ public class Testing_Sprint2_Day3_model_Offer {
 	public void init()
 	{
 		offer1 = new Offer(1,"Angel","88003553555" , "Angel.ingeras@gmail.com", "Eu vreu sifer calitativ, da ieftin , totusi is roman huli");
-		offer2 = new Offer(2,"Andrei","6666666" , "Andrei.hellMaster@gmail.com", "Dieeee!!! ,, You all will dieeeee !!!!");
-		offer3 = new Offer(3,"Andrei","6666666" , "Andrei.hellMaster@gmail.com", "Dieeee!!! ,, You all will dieeeee !!!!");
+		offer2 = new Offer(2,"","6666666" , "Andrei.hellMaster@gmail.com", "Dieeee!!! ,, You all will dieeeee !!!!");
+		offer3 = new Offer(3,"Andrei","666642666" , "andrei", "Dieeee!!! ,, You all will dieeeee !!!!");
 	}
 	@Test
 public void getID()
@@ -28,5 +28,20 @@ public void setID()
 {
 	offer1.setID(6);
    assertEquals(6,offer1.getID());
+}
+@Test
+public void checkFormat()
+{
+	assertEquals(4,offer1.checkFormat());
+}
+@Test
+public void checkFormat1()
+{
+	assertEquals(0,offer2.checkFormat());
+}
+@Test
+public void checkFormat2()
+{
+	assertEquals(2,offer3.checkFormat());
 }
 }
--- src/testing/Testing_Sprint2_Day3_model_Store.java
+++ src/testing/Testing_Sprint2_Day3_model_Store.java
@@ -1,5 +1,28 @@
 package testing;
 
-public class Testing_Sprint2_Day3_model_Store {
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
+
+import model.Store;
+
+public class Testing_Sprint2_Day3_model_Store {
+	private Store store;
+	   
+	   @Before
+	   public void setup()
+	   {
+	      store = new Store();
+	   }
+	
+	@Test
+	public void testGenerateProductID() {
+		assertEquals(1,store.generateProductID());
+	}
+
+	@Test
+	public void testGenerateOfferID() {
+		assertEquals(1,store.generateOfferID());
+	}
 }
