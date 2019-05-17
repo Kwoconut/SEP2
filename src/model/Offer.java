065b0b3bd05b1b361dd21159135476215bbfb4df
Kwoconut <43205572+Kwoconut@users.noreply.github.com>
2019-05-17 13:48:21 +0200

    update

--- src/model/Offer.java
+++ src/model/Offer.java
@@ -20,27 +20,9 @@ public class Offer implements Serializable
       this.message = message;
    }
 
-   public Offer(String name, String phoneNo, String email, String message)
-   {
-      this.name = name;
-      this.phoneNo = phoneNo;
-      this.email = email;
-      this.message = message;
-   }
-
    public int getID()
    {
       return ID;
-   }
-
-   public void setID(int iD)
-   {
-      ID = iD;
-   }
-
-   public void setName(String name)
-   {
-      this.name = name;
    }
 
    public String getName()
@@ -48,29 +30,14 @@ public class Offer implements Serializable
       return name;
    }
 
-   public void setPhoneNo(String phoneNo)
-   {
-      this.phoneNo = phoneNo;
-   }
-
    public String getPhoneNo()
    {
       return phoneNo;
    }
 
-   public void setEmail(String email)
-   {
-      this.email = email;
-   }
-
    public String getEmail()
    {
       return email;
-   }
-
-   public void setMessage(String message)
-   {
-      this.message = message;
    }
 
    public String getMessage()
