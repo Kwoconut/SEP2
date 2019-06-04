d6db5b617c422c545c9cca4ce55c73378b57c880
joelh273 <45686194+joelh273@users.noreply.github.com>
2019-06-04 14:07:19 +0200

    /ass

--- src/testing/ReviewTest.java
+++ src/testing/ReviewTest.java
@@ -17,44 +17,37 @@ public class ReviewTest {
 	private Product product;
 	private Product product2;
 	private Product product3;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		product = new Product(1, "Denise Metal Tile", 200, "orange",
-	            "Metal Tile", "deniseMetalTile.png");
-		product2 = new Product(2, "Queen Metal Tile", 300, "orange",
-	            "Metal Tile", "deniseMetalTile.png");
-		product3 = new Product(3, "Teenager Metal Tile", 400, "orange",
-	            "Metal Tile", "deniseMetalTile.png");
-		review = new Review(1,5,"Very good roof",product);
-		review2 = new Review(2,4,"Nice big roof",product2);
-		review3 = new Review(3,3,"Bad roof could be better",product3);
-		review4 = new Review(3,3,"Bad roof could be better",product3);
+		product = new Product(1, "Denise Metal Tile", 200, "orange", "Metal Tile", "deniseMetalTile.png");
+		product2 = new Product(2, "Queen Metal Tile", 300, "orange", "Metal Tile", "deniseMetalTile.png");
+		product3 = new Product(3, "Teenager Metal Tile", 400, "orange", "Metal Tile", "deniseMetalTile.png");
+		review = new Review(1, 5, "Very good roof", product);
+		review2 = new Review(2, 4, "Nice big roof", product2);
+		review3 = new Review(3, 3, "Bad roof could be better", product3);
+		review4 = new Review(3, 3, "Bad roof could be better", product3);
+		
 	}
 
 	@Test
 	public void testGetRating() {
-		assertEquals(5,review.getRating(),0);
+		assertEquals(5, review.getRating(), 0);
 	}
 
 	@Test
 	public void testGetMessage() {
-		assertEquals("Nice big roof",review2.getMessage());
+		assertEquals("Nice big roof", review2.getMessage());
 	}
 
 	@Test
 	public void testGetProduct() {
-		assertEquals(product2,review2.getProduct());
+		assertEquals(product2, review2.getProduct());
 	}
 
 	@Test
 	public void testGetID() {
-		assertEquals(2,review2.getID());
-	}
-
-	@Test
-	public void testEqualsObject() {
-		assertEquals(true,review3.equals(review4));
+		assertEquals(2, review2.getID());
 	}
 
 }
